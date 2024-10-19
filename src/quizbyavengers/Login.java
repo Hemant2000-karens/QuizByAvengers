8a2772903cf20ddd2ad06297f8be4bb0b92eb10f
Hemant Kumar <team.karensbook@icloud.com>
2024-10-19 19:25:16 +0530

    nominal

--- src/quizbyavengers/Login.java
+++ src/quizbyavengers/Login.java
@@ -6,10 +6,8 @@
 package quizbyavengers;
 
 //import java.awt.Dimension;
-import java.awt.Toolkit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JOptionPane;
 
 
 /**
