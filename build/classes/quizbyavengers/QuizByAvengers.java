592f0894fc2bf9e9ae56dccad1241c3f1e9a3a3d
Hemant Kumar <team.karensbook@icloud.com>
2024-10-19 18:25:36 +0530

    initial comit

---
+++ build/classes/quizbyavengers/QuizByAvengers.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ * Project QUIZ
+ * 
+ * Class- 12 'B'
+ * Group Members:
+ * 
+ * S.No.    Roll No.    Name
+ *  1.        06        Abhishek
+ *  2.        08        Ankit Anand Singh
+ *  3.        11        Anupam
+ *  4.        12        Anurag Singh
+ *  5.        18        Gyan Prakash
+ *  6.        20        Hemant Kumar
+ *  7.        47        Shival Gupta
+ * 
+ */
+
+package quizbyavengers;
+/**
+ * @author Shival_Gupta
+ */
+public class QuizByAvengers {
+    
+    public static void main(String[] args) {
+        Login Login= new Login();
+        Login.setVisible(true);
+    }
+    
+}
---
+++ build/classes/quizbyavengers/SignUp.java
@@ -0,0 +1,256 @@
+/**
+ * 
+ * Project QUIZ
+ * 
+ * Class- 12 'B'
+ * Group Members:
+ * 
+ * S.No.    Roll No.    Name
+ *  1.        06        Abhishek
+ *  2.        08        Ankit Anand Singh
+ *  3.        11        Anupam
+ *  4.        12        Anurag Singh
+ *  5.        18        Gyan Prakash
+ *  6.        20        Hemant Kumar
+ *  7.        47        Shival Gupta
+ * 
+ */
+
+
+package quizbyavengers;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author Shival_Gupta
+ */
+public class SignUp extends javax.swing.JFrame {
+    
+    public SignUp() {
+        initComponents();
+    }
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        main = new javax.swing.JPanel();
+        background = new keeptoo.KGradientPanel();
+        bgsignup = new keeptoo.KGradientPanel();
+        signupbtn = new keeptoo.KGradientPanel();
+        redirect2signin = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        lheading = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Sign Up");
+        setUndecorated(true);
+        setResizable(false);
+
+        main.setBackground(new java.awt.Color(255, 255, 255));
+
+        background.setkEndColor(new java.awt.Color(102, 0, 255));
+        background.setkGradientFocus(1800);
+        background.setkStartColor(new java.awt.Color(240, 153, 50));
+
+        bgsignup.setkEndColor(new java.awt.Color(255, 255, 204));
+        bgsignup.setkStartColor(new java.awt.Color(255, 255, 255));
+
+        signupbtn.setkEndColor(new java.awt.Color(0, 102, 102));
+        signupbtn.setkGradientFocus(10);
+        signupbtn.setkStartColor(new java.awt.Color(0, 102, 51));
+
+        javax.swing.GroupLayout signupbtnLayout = new javax.swing.GroupLayout(signupbtn);
+        signupbtn.setLayout(signupbtnLayout);
+        signupbtnLayout.setHorizontalGroup(
+            signupbtnLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        signupbtnLayout.setVerticalGroup(
+            signupbtnLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 49, Short.MAX_VALUE)
+        );
+
+        redirect2signin.setBackground(new java.awt.Color(255, 255, 223));
+        redirect2signin.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                redirect2signinMouseReleased(evt);
+            }
+        });
+
+        jLabel1.setForeground(new java.awt.Color(51, 0, 0));
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.TRAILING);
+        jLabel1.setText("Already have an Account?");
+
+        jLabel2.setForeground(new java.awt.Color(0, 105, 210));
+        jLabel2.setText("Sign in");
+
+        javax.swing.GroupLayout redirect2signinLayout = new javax.swing.GroupLayout(redirect2signin);
+        redirect2signin.setLayout(redirect2signinLayout);
+        redirect2signinLayout.setHorizontalGroup(
+            redirect2signinLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(redirect2signinLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 136, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(18, 18, 18))
+        );
+        redirect2signinLayout.setVerticalGroup(
+            redirect2signinLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(redirect2signinLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+
+        lheading.setFont(new java.awt.Font("Yu Gothic UI", 1, 36)); // NOI18N
+        lheading.setForeground(new java.awt.Color(0, 76, 70));
+        lheading.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lheading.setText("LOGIN");
+        lheading.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+
+        javax.swing.GroupLayout bgsignupLayout = new javax.swing.GroupLayout(bgsignup);
+        bgsignup.setLayout(bgsignupLayout);
+        bgsignupLayout.setHorizontalGroup(
+            bgsignupLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(bgsignupLayout.createSequentialGroup()
+                .addGap(104, 104, 104)
+                .addComponent(redirect2signin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(bgsignupLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(bgsignupLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(signupbtn, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(lheading, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+        bgsignupLayout.setVerticalGroup(
+            bgsignupLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, bgsignupLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(lheading, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 335, Short.MAX_VALUE)
+                .addComponent(signupbtn, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(105, 105, 105)
+                .addComponent(redirect2signin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+
+        jLabel3.setFont(new java.awt.Font("Verdana", 1, 24)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(255, 0, 0));
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Resources/Close1.png"))); // NOI18N
+        jLabel3.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                jLabel3MouseReleased(evt);
+            }
+        });
+
+        javax.swing.GroupLayout backgroundLayout = new javax.swing.GroupLayout(background);
+        background.setLayout(backgroundLayout);
+        backgroundLayout.setHorizontalGroup(
+            backgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(backgroundLayout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(bgsignup, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 286, Short.MAX_VALUE)
+                .addComponent(jLabel3))
+        );
+        backgroundLayout.setVerticalGroup(
+            backgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(backgroundLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(bgsignup, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+            .addGroup(backgroundLayout.createSequentialGroup()
+                .addComponent(jLabel3)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout mainLayout = new javax.swing.GroupLayout(main);
+        main.setLayout(mainLayout);
+        mainLayout.setHorizontalGroup(
+            mainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(background, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        mainLayout.setVerticalGroup(
+            mainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(background, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(main, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(main, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        setSize(new java.awt.Dimension(751, 600));
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void redirect2signinMouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_redirect2signinMouseReleased
+        Login lgWin = new Login();
+        lgWin.setVisible(true);
+        this.setVisible(false);
+        
+    }//GEN-LAST:event_redirect2signinMouseReleased
+
+    private void jLabel3MouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel3MouseReleased
+        this.dispose();
+        try {
+            Thread.sleep(10000);
+            System.exit(0);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }//GEN-LAST:event_jLabel3MouseReleased
+
+    public static void main(String args[]) {
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(SignUp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(SignUp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(SignUp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(SignUp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new SignUp().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private keeptoo.KGradientPanel background;
+    private keeptoo.KGradientPanel bgsignup;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel lheading;
+    private javax.swing.JPanel main;
+    private javax.swing.JPanel redirect2signin;
+    private keeptoo.KGradientPanel signupbtn;
+    // End of variables declaration//GEN-END:variables
+}
