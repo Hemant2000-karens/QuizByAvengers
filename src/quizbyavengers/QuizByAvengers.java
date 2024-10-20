b64f6609c6f6a05fec16ca0baf2297eb430c7729
Hemant Kumar <team.karensbook@icloud.com>
2024-10-20 22:52:48 +0530

    code update
    Fixed and upgraded code writing style, and minimized new instances creation in Main File, library, replaced multi-catch all wheres.

--- src/quizbyavengers/QuizByAvengers.java
+++ src/quizbyavengers/QuizByAvengers.java
@@ -22,9 +22,13 @@ package quizbyavengers;
  */
 public class QuizByAvengers {
     
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-        Login Login= new Login();
-        Login.setVisible(true);
+        new Login().setVisible(true);
+        
     }
     
 }
